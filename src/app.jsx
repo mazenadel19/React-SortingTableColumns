f77de4a6c8298891801372590c6d99023da77847
Mazen <mazenadel19@gmail.com>
2022-03-12 19:30:38 +0200

    Handling edge cases, Similar Order number & no order number

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,10 @@ import { useEffect, useState } from 'react';
 const HeadersSchema = [
 	{
 		name: 'userId',
-		order: 88,
+		order: 2,
 	},
 	{
 		name: 'id',
-		order: 6,
 	},
 	{
 		name: 'title',
@@ -22,19 +21,24 @@ const HeadersSchema = [
 export function App() {
 	const [Data, setData] = useState([]);
 	const [TableHeaders, setTableHeaders] = useState([]);
-	console.log(TableHeaders); //(4) ["userId", "id", "title", "completed"]
 
   useEffect(() => {
-    
+
 		const fetchData = async () => {
 			const response = await fetch('https://jsonplaceholder.typicode.com/todos');
 			const json = await response.json();
 			const TableHeaders = [];
 
+			const HandlingHeaders = Num => {
+				if (!TableHeaders[Num]) return Num;
+				else return HandlingHeaders(Num + 1);
+			};
+
 			Object.keys(json[0]).forEach(Key => {
 				HeadersSchema.forEach(Header => {
 					if (Header.name === Key) {
-						TableHeaders[Header.order] = Key;
+						const HeaderOrder = HandlingHeaders(Header.order ? Header.order : 99);
+						TableHeaders[HeaderOrder] = Key;
 					}
 				});
 			});
@@ -50,7 +54,9 @@ export function App() {
 			<thead>
 				<tr>
 					{TableHeaders.map((Header, Index) => (
-						<th scope='col' key={Index}>{Header}</th>
+						<th scope='col' key={Index}>
+							{Header}
+						</th>
 					))}
 				</tr>
 			</thead>
